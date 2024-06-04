93165bbd574fec1c25fbd1124c86669ad81cc201
M-Jeffryes <michael@maleboxhealth.com>
2024-06-04 16:10:32 +0100

    rename package

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 from os import path
+import versioneer
 
 work_dir = path.abspath(path.dirname(__file__))
 
@@ -7,8 +8,8 @@ with open(path.join(work_dir, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="andrology-statistics",
-    version="0.1.1",
+    name="AndroStats",
+    version="0.1.5",
     description="Stats for andrology data",
     long_description=long_description,
     long_description_content_type="text/markdown",
