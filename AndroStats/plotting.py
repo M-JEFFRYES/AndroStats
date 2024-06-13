0d879a58b6576e25101b1e995cf2a32ad0d6fc78
M-Jeffryes <michael@maleboxhealth.com>
2024-06-13 14:47:41 +0100

    Typecheck and lint fix

--- AndroStats/plotting.py
+++ AndroStats/plotting.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
-import seaborn as sns
+from matplotlib.figure import Figure
+import seaborn as sns  # type: ignore
 import pandas as pd
+import numpy as np
+from numpy.typing import NDArray
+from typing import Dict, Any
 from AndroStats.performance import BlandAltmanCalculation
 
 
-def plot_prediction_comparision(df: pd.DataFrame, true_label: str, prediction_label: str) -> plt.figure:
+def plot_prediction_comparision(df: pd.DataFrame, true_label: str, prediction_label: str) -> Figure:
     fig, ax = plt.subplots(figsize=(10, 6))
     sns.scatterplot(data=df, x=true_label, y=prediction_label, ax=ax)
     ax.set_title(f"{prediction_label} vs {true_label}")
@@ -13,12 +17,12 @@ def plot_prediction_comparision(df: pd.DataFrame, true_label: str, prediction_la
     return fig
 
 
-def bland_altman(df: pd.DataFrame, true_label: str, prediction_label: str) -> plt.figure:
-    bac = BlandAltmanCalculation()
+def bland_altman(df: pd.DataFrame, true_label: str, prediction_label: str) -> Figure:
+    bac: BlandAltmanCalculation = BlandAltmanCalculation()
 
-    true_values = df[true_label].values
-    prediction_values = df[prediction_label].values
-    bland_altman_data = bac.calculate(true_values=true_values, predicted_values=prediction_values)
+    true_values: NDArray[np.float64] = df[true_label].to_numpy()
+    prediction_values: NDArray[np.float64] = df[prediction_label].to_numpy()
+    bland_altman_data: Dict[str, Any] = bac.calculate(true_values=true_values, predicted_values=prediction_values)
 
     df["mean"] = bland_altman_data["mean"]
     df["difference"] = bland_altman_data["diff"]
